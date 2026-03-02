35fd70d5aee3ed4b56d498426b85e16aa23e0995
matheuscdd <matheuscdd@hotmail.com>
2026-03-02 00:21:39 -0300

    feat: add transcriptions and new chapters

--- convertAudio.js
+++ convertAudio.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 const ffmpeg = require("fluent-ffmpeg");
 const ffmpegPath = require("ffmpeg-static");
 
